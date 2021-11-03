0a6abb3205692d496a3564ed3bff8f1d1e6cc974
Akshay V <ak.andcompany911@gmail.com>
2021-11-03 15:07:00 +0530

    making a matrix

---
+++ polymatlab.py
@@ -0,0 +1,7 @@
+import numpy as np
+a = int(input("Enter the number of rows:"))
+b = int(input("Enter the number of columns:"))
+print("Enter the number in a single line separated by space:")
+val = list(map(int, input().split()))
+matrix = np.array(val).reshape(a,b)
+print(matrix)
